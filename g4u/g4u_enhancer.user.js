69f69ca24fbe474d170db998b0c6a2156441d4ec
BEY0NDER <bey0nder@pm.me>
2025-03-14 13:45:31 +0200

    version change

--- g4u/g4u_enhancer.user.js
+++ g4u/g4u_enhancer.user.js
@@ -1,6 +1,6 @@
 ﻿// ==UserScript==
 // @name         G4U Enhancer
-// @version      1.0
+// @version      1.0.1
 // @description  Removes promotional elements, nullifies openPopup function, and adds dark mode to g4u.to
 // @author       BEY0NDER
 // @match        *://g4u.to/*
